0947e33179af9b58a6b851dfd6b3029505d97653
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-06 15:54:53 +0100

    Dodanie przykładu kodowania OneHotEncoding

---
+++ src/Encoding/OneHotEncoding/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.ML;
+
+Console.WriteLine("Hello, One Hot Encoding!");
+
+var context = new MLContext();
+
+var data = new CategoryData[]
+{
+    new CategoryData { Category = "kot"},
+    new CategoryData { Category = "pies"},
+    new CategoryData { Category = "ryba"},
+    new CategoryData { Category = "kaczka"},
+    new CategoryData { Category = "pies"},
+};
+
+
+var dataView = context.Data.LoadFromEnumerable(data);
+
+var pipeline = context.Transforms.Categorical.OneHotEncoding("EncodedCategory", "Category");
+
+var transformer = pipeline.Fit(dataView);
+var transformedData = transformer.Transform(dataView);
+
+var encodedData = context.Data.CreateEnumerable<CategoryEncoded>(transformedData, reuseRowObject: false);
+
+
+foreach (var item in encodedData)
+{
+    Console.WriteLine($"Category: {item.Category} One hot encoding: [ {string.Join(", ",  item.EncodedCategory)} ]");
+}
+
+Console.ReadLine();
+
+public class CategoryData
+{
+    public string Category { get; set; }
+}
+
+
+public class CategoryEncoded : CategoryData
+{
+    public float[] EncodedCategory { get; set; }
+}
